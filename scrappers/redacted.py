7f5f833ce548a9dcf86890541c1d78090f907fe1
Appolon24800 <contact@appolon.dev>
2026-06-05 02:50:23 +0200

    fix: cleaned up redacted.py code

--- scrappers/redacted.py
+++ scrappers/redacted.py
@@ -13,38 +13,24 @@ USER_STATS_URL = "https://redacted.sh/ajax.php?action=index"
 
 
 async def get_stats(_: bool) -> Dict[str, Any]:
+    token = os.getenv("RED_APIKEY")
+    if not token:
+        raise MissingCredentialsError("Missing Redacted api key")
+
     try:
-        res: Dict[str, Any] = {"ratio": "N/A", "upload": "N/A", "download": "N/A"}
-
-        token = os.getenv("RED_APIKEY")
-        if not token:
-            raise MissingCredentialsError("Missing Redacted api key")
-
-        try:
-            req = urllib.request.Request(USER_STATS_URL, headers={"User-Agent": default_user_agent, "Authorization": token})
-            with urllib.request.urlopen(req) as response:
-                api_data = json.loads(response.read())
-            
-        except urllib.error.HTTPError as e:
-            raise ScrappingError(f"Failed to get Redacted stats: HTTP {e.code}, Reason {e.reason}")
-        except urllib.error.URLError as e:
-            raise ScrappingError(e)
-
-        status = api_data.get("status", "0")
-        if status != "success":
-            raise ScrappingError(api_data.get("error", f"Scrapping error from redacted '{api_data}'"))
-
-        api_response = api_data.get("response", "0")
-        userstats = api_response["userstats"]
-
-        res["raw_upload"] = userstats["uploaded"]
-        res["raw_download"] = userstats["downloaded"]
-        #res["ratio"] = userstats["ratio"]
-        res["bonus"] = 0 # No bonus system on redacted
-
-        return res
-
-    except (MissingCredentialsError, ScrappingError):
-        raise
-    except Exception as e:
+        req = urllib.request.Request(USER_STATS_URL, headers={"User-Agent": default_user_agent, "Authorization": token})
+        with urllib.request.urlopen(req) as response:
+            api_data = json.loads(response.read())
+        
+    except urllib.error.HTTPError as e:
+        raise ScrappingError(f"Failed to get Redacted stats: HTTP {e.code}, Reason {e.reason}")
+    except urllib.error.URLError as e:
         raise ScrappingError(e)
+
+    status = api_data.get("status")
+    if status != "success":
+        raise ScrappingError(api_data.get("error", f"Scrapping error from redacted '{api_data}'"))
+
+    res = api_data.get("response").get("userstats")
+
+    return {"raw_upload": res.get("uploaded"), "raw_download": res.get("downloaded"), "bonus": 0}
